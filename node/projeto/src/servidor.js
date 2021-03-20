2d229cb7453f47cdb4ca5328960776e83b59cb15
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-03-19 22:58:34 -0300

    express exercicios

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,12 +2,18 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+//const bodyParser = require('body-parser')
 const bancoDeDados = require('./bancoDeDados')
+
+app.use(express.json())
+
+app.use(express.urlencoded());
 
 /*app.get('/produtos', (req, res, next) => {
   console.log('Middleware 1...')
   next()
 })*/
+
 
 app.get('/produtos', (req, res, next) => {
   res.send(bancoDeDados.getProdutos())
@@ -19,9 +25,23 @@ app.get('/produtos/:id', (req, res, next) => {
 
 app.post('/produtos', (req, res, next) => {
   const produto = bancoDeDados.salvarProduto({
-    nome: req.body.name,
+    nome: req.body.nome,
     preco: req.body.preco
   })
+  res.send(produto) // JSON
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+  const produto = bancoDeDados.salvarProduto({
+    id: req.params.id,
+    nome: req.body.nome,
+    preco: req.body.preco
+  })
+  res.send(produto) // JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+  const produto = bancoDeDados.excluirProduto(req.params.id)
   res.send(produto) // JSON
 })
 
