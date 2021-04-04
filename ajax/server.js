89efde6a36a38a9ffd3d1bac423b2bdc6486d282
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-04-04 12:44:38 -0300

    ajax

--- ajax/server.js
+++ ajax/server.js
@@ -26,4 +26,11 @@ app.post('/upload', (req, res) => {
   res.end('Concluído com sucesso.')
 })
 
+app.post('/formulario', (req, res) => {
+  res.send({
+    ...req.body,
+    id: 7
+  })
+})
+
 app.listen(8081, () => console.log('Executando...'))
