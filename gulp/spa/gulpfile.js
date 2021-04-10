d8d6e99a3717afa1c200c6fff41dc8deebbd89b6
Felipe Gomes Vicente <61098587+felipe-gomes-vicente@users.noreply.github.com>
2021-04-09 22:21:34 -0300

    Update gulpfile.js

--- gulp/spa/gulpfile.js
+++ gulp/spa/gulpfile.js
@@ -1,4 +1,4 @@
-const { series, parallel} = require('gulp')
+const {series, parallel} = require('gulp')
 const gulp = require('gulp')
 
 const { appHTML, appCSS, appJS, appIMG } = require('./gulpTasks/app')
